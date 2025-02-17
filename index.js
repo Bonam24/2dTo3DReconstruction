1a80cb52450ea88051d2664f11ed1eb3b981d187
Bonam24 <bosene@usiu.ac.ke>
2025-02-17 04:59:31 +0300

    Change the port

--- index.js
+++ index.js
@@ -43,6 +43,8 @@ app.post("/generate-mesh", (req, res) => {
 
 
 
-app.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
 });
